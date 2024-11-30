54e81d1e6a73a1aec8e02e52c74d6e72b8ea04ce
sujitsingh08rdm <sujitsingh08rdm@gmail.com>
2024-11-30 15:38:39 +0530

    made this project prod deployment ready

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import path from "path";
 import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 app.use(express.json()); // allows us to accept JSON data in body
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 // console.log(process.env.MONGO_URI);
 
